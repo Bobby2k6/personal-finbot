b50916261b2558d9f7312a2a69320ca1994f886f
M V Karthikeyan <147787555+Bobby2k6@users.noreply.github.com>
2025-08-02 11:06:40 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ import os
 # DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./financebot.db")
 
 # For PostgreSQL in production, use:
-DATABASE_URL = "postgresql://finbot_user:cobWsVmqSkJ1l4rWe50Rkia8D9jFW9T5@dpg-d26jd6re5dus73b6d120-a.oregon-postgres.render.com/finbot_db"/
+DATABASE_URL = "postgresql://finbot_user:cobWsVmqSkJ1l4rWe50Rkia8D9jFW9T5@dpg-d26jd6re5dus73b6d120-a.oregon-postgres.render.com/finbot_db"
 
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
